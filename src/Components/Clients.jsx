ae21b4ae5e809dd78e039501430db2f94046c295
Sargun <sargun.kohli152@gmail.com>
2024-02-11 03:32:22 +0530

    first commit

---
+++ src/Components/Clients.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {clients} from "../constants";
+import styles from '../style';
+
+const Clients = () => {
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+            <img key={client.id} src={client.logo} alt="client"
+              className='sm:w-[192px] w-[100px] object-contain'
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default Clients
